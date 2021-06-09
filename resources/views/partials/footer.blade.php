92a8546e19366b36a5883a0c6b3062392096c302
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-06-09 09:18:55 +0200

    .

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,6 +2,16 @@
 
 <footer>
     <section class="container">
-        
+        <div class=" footer wrapper flex">
+            <button class="footer-button">SIGN UP NOW!</button>
+            <div class="filler"></div>
+            <ul class=" socialmedia-icons flex">
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>YouTube</li>
+                <li>Pinterest</li>
+                <li>?</li>
+            </ul>
+        </div> <!-- Wrapper -->
     </section>
 </footer>
